12d959b5ef069b03b2b42ec8d8d449006a642d58
cooperhankun <cooperhanson@gmail.com>
2021-11-18 19:51:17 +0100

    save the dashboard.py and .ipynb deleted

--- Code/dashboard.py
+++ Code/dashboard.py
@@ -14,13 +14,13 @@ os_medal_event = players_os.drop_duplicates(subset=["Event", "Year", "Medal"])
 # China team data collections:
 China_team = players_os[players_os["Team"] == "China"]
 China_medal = China_team.drop_duplicates(subset=["Event", "Year", "Medal"])
-China_most_gold = pd.DataFrame(China_medal["Sport"][China_medal["Medal"] == "Gold"].value_counts().head())
+China_most_gold = pd.DataFrame(China_medal["Sport"][China_medal["Medal"] == "Gold"].value_counts().head(10))
 China_most_medal = China_medal.dropna(subset=['Medal'])
-China_most_medal = pd.DataFrame(China_most_medal.Sport.value_counts().head())
+China_most_medal = pd.DataFrame(China_most_medal.Sport.value_counts().head(10))
 china_medal_year = China_medal.groupby(["Year"]).count().reset_index()
 most_gold_China = China_team[["Name", "Sport"]][China_team["Medal"] == "Gold"].value_counts().head(10)
 most_gold_China = pd.DataFrame(most_gold_China.sort_values(ascending=False).reset_index())
-china_age = pd.DataFrame(China_team.Age.reset_index())
+china_age = China_team[["Age", "Sex"]].reset_index()
 
 # Sports statistic data collections:
 sport_evelution = players_os.groupby(["City", "Year","Season"]).Sport.nunique().reset_index()
@@ -37,10 +37,6 @@ Ice_Hockey = SportFig(os_sport_medal, "Ice Hockey")
 china_dict = dict(Medal="Medal", Gold="Gold", Players="Players", Age="Age")
 china_options_dropdown = [{"label": name, "value": symbol}
                           for symbol, name in china_dict.items()]
-
-sport_dict = dict(Football="Football", Swimming="Swimming", Hockey="Hockey")
-sport_options_dropdown = [{"label": name, "value": symbol}
-                          for symbol, name in sport_dict.items()]
 
 options_dict =dict(Gender="Gender", Country="Country", Age="Age")
 attributes_options_dropdown = [{"label": name, "value": symbol}
@@ -94,6 +90,7 @@ app.layout = dbc.Container([
  #-------------------------------------------------------------------------------------------------------
     
     
+    
     dbc.Col(
             html.P("Choose options for a football figures: "), className="mt-1", style={'color':'grey'},
                 lg="4", xl={"size": 6, "offset": 0.5}),
@@ -101,12 +98,40 @@ app.layout = dbc.Container([
     dbc.Col(
             dcc.Dropdown(id='football-dropdown', className='',
                          options=attributes_options_dropdown,
-                         value= "Gender"), lg="4", xl="3"),
-
-    dbc.Col(
-            dcc.Graph(id='football-graph', className=''))
+                         value= "Gender"),lg="4", xl="3"),
+    dbc.Row([
+        dbc.Col(
+                dcc.Graph(id='football-graph', className=''))
+            ]),
             
-        
+            
+    dbc.Row(
+        dbc.Col(
+            html.P("Choose options for a swimming figures: "), className="mt-1", style={'color':'grey'},
+                lg="4", xl={"size": 6, "offset": 0.5})),
+        dbc.Col(
+            dcc.Dropdown(id='swimming-dropdown', className='',
+                    options=attributes_options_dropdown,
+                    value= "Country"),lg="4", xl="3"),
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(id='swimming-graph', className=''))
+            ]),
+
+
+    dbc.Row(
+        dbc.Col(
+            html.P("Choose options for a Ice-Hockey figures: "), className="mt-1", style={'color':'grey'},
+                lg="4", xl={"size": 6, "offset": 0.5})),
+                
+        dbc.Col(
+            dcc.Dropdown(id='Hockey-dropdown', className='',
+                    options=attributes_options_dropdown,
+                    value= "Age"),lg="4", xl="3"),
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(id='Hockey-graph', className=''))
+            ]),
 
 
             
@@ -122,7 +147,7 @@ def update_graph(value):
     Medal= px.bar(China_most_medal, x=China_most_medal.index, y="Sport", title="Most Medal sports, China")
     Gold = px.bar(China_most_gold, x=China_most_gold.index, y="Sport", title="Most gold sports, China")
     Players = px.bar(most_gold_China, x="Name", y=0, title="Top gold gained players") 
-    Age = px.histogram(china_age, x="Age", title="Histgram of age from China team")
+    Age = px.histogram(china_age, x="Age", color="Sex", title="Histgram of age from China team")
 
     if value == "Medal":
         return Medal
@@ -148,16 +173,50 @@ def update_graph(Year):
     return fig
 
 
-
 @app.callback(
     Output("football-graph", "figure"),
     Input("football-dropdown", "value"))
 
-def update_graph(value):
+def update_graph(football_value):
     
     fig1=football.sport_gender()
     fig2=football.sport_country()
     fig3=football.sport_age()
+
+    if football_value == "Gender":
+        return fig1
+    if football_value == "Country":
+        return fig2
+    if football_value == "Age":
+        return fig3
+
+@app.callback(
+    Output("swimming-graph", "figure"),
+    Input("swimming-dropdown", "value"))
+
+def update_graph(value):
+
+    fig1=Swimming.sport_gender()
+    fig2=Swimming.sport_country()
+    fig3=Swimming.sport_age()
+
+    if value == "Gender":
+        return fig1
+    if value == "Country":
+        return fig2
+    if value == "Age":
+    
+        return fig3
+
+@app.callback(
+    Output("Hockey-graph", "figure"),
+    Input("Hockey-dropdown", "value"))
+
+def update_graph(value):
+
+    fig1=Ice_Hockey.sport_gender()
+    fig2=Ice_Hockey.sport_country()
+    fig3=Ice_Hockey.sport_age()
 
     if value == "Gender":
         return fig1
@@ -165,9 +224,6 @@ def update_graph(value):
         return fig2
     if value == "Age":
         return fig3
-
-
-
 
 if __name__ == "__main__":
     app.run_server(debug=False)
